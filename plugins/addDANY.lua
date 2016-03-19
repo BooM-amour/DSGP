4c5884a5959c3882c6768c1c70371088d5998769
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-19 15:37:16 -0700

    Update addDANY.lua

--- plugins/addDANY.lua
+++ plugins/addDANY.lua
@@ -5,7 +5,7 @@ local function callback(extra, success, result)
 end
     function run(msg, matches)
         if not is_momod or not is_owner then
-    return "Only Onwers Can Add iman!"
+    return "Only Onwers Can Add DANY!"
 end
     local user = 'user#id124464490'
     local chat = 'chat#id'..msg.to.id
