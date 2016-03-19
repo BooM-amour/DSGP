904097f471374eff72a290a998491da150f92585
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-19 15:37:36 -0700

    Update addKING.lua

--- plugins/addKING.lua
+++ plugins/addKING.lua
@@ -5,7 +5,7 @@ local function callback(extra, success, result)
 end
     function run(msg, matches)
         if not is_momod or not is_owner then
-    return "Only Onwers Can Add iman!"
+    return "Only Onwers Can Add KING!"
 end
     local user = 'user#id210948526'
     local chat = 'chat#id'..msg.to.id
